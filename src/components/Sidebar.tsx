983ac2c91e210c657f1367d19f98a17c3606e652
mohith <mohithpeta@gmail.com>
2025-03-29 14:52:42 +0530

    changed theme color and added logo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Home, GraduationCap, MessageCircle, User, Radio, Bell, LayoutDashboard} from 'lucide-react';
+import LifecourseLogo from '../assets/Lifecourse Logo.png';
 
 interface NavLinkProps {
   icon: React.ReactNode;
@@ -17,7 +18,7 @@ function NavLink({ icon, label, to, isActive }: NavLinkProps) {
       onClick={() => navigate(to)}
       className={`w-full flex items-center px-4 py-2 rounded-md transition-colors ${
         isActive
-          ? 'text-[#A32E76] bg-pink-50'
+          ? 'text-[#5E17EB] bg-pink-50'
           : 'text-gray-700 hover:bg-gray-100'
       }`}
     >
@@ -34,21 +35,22 @@ export function Sidebar() {
   return (
     <div className="w-64 bg-white h-screen border-r border-gray-200 p-4">
       <div className="mb-8">
-        <h1 className="text-xl font-semibold text-[#A32E76]">LifeCourse®Experts</h1>
+        <img src={LifecourseLogo} alt="Lifecourse Logo" className="w-36 h-auto" />
       </div>
       <nav className="space-y-2">
+      <NavLink
+          icon={<Home className="w-5 h-5" />}
+          label="Home"
+          to="/home"
+          isActive={currentPath === '/home'}
+        />
         <NavLink
           icon={<LayoutDashboard className="w-5 h-5" />} 
           label="Dashboard"
           to="/my-dashboard"
           isActive={currentPath === '/my-dashboard'}
         />
-        <NavLink
-          icon={<Home className="w-5 h-5" />}
-          label="Home"
-          to="/home"
-          isActive={currentPath === '/home'}
-        />
+        
         <NavLink
           icon={<GraduationCap className="w-5 h-5" />}
           label="LifeCourse Experts"
